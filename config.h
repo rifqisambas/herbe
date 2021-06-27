edb0fe62e8430a01d7f5982dd432594739a386d4
smfcz <rssr@tuta.io>
2021-06-27 07:22:51 +0700

    :wrench: add my config

---
+++ config.h
@@ -0,0 +1,19 @@
+static const char *background_color = "#000000";
+static const char *border_color = "#e0e0e0";
+static const char *font_color = "#e0e0e0";
+static const char *font_pattern = "Hermit:size=10";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 10;
+
+static const unsigned int width = 388;
+static const unsigned int border_size = 0;
+static const unsigned int pos_x = 0;
+static const unsigned int pos_y = 0;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 3; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
